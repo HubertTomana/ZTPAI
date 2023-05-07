58afc1d36f5de21b15caea63d16c64d2e722bae0
Hubert Tomana <hubertomana@gmail.com>
2023-05-08 00:23:14 +0200

    Spring conf + github

---
+++ src/test/java/com/example/RecipeBookApp/RecipeBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
